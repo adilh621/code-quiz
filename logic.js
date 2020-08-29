0b3f572c89d85274a769966ce873eff225af644b
Adil Hussain <adilh621@gmail.com>
2020-08-29 19:30:57 -0400

    Added JS for our startquiz function end quiz function and grabbing question and choices and displaying it

--- logic.js
+++ logic.js
@@ -11,31 +11,64 @@ var submitBtn = document.getElementById("submit");
 var startBtn = document.getElementById("start");
 var initialsEl = document.getElementById("initials");
 var feedbackEl = document.getElementById("feedback");
+var titleScreen = document.querySelector("#title-section");
+var questionScreen = document.querySelector("#questions");
+var endScreen = document.querySelector("#end-screen");
+var questionTitle = document.querySelector("#question-title");
+
 
 // sound effects
-// var sfxRight = new Audio("assets/sfx/correct.wav");
-// var sfxWrong = new Audio("assets/sfx/incorrect.wav");
+var sfxRight = new Audio("assets/correct.wav");
+var sfxWrong = new Audio("assets/incorrect.wav");
 
 function startQuiz() {
   // hide start screen
 
+  titleScreen.setAttribute("class" , "hide");
+  
   // un-hide questions section
+
+  questionScreen.setAttribute("class" , "show")
 
   // start timer
 
+  timerId = setInterval(clockTick , 1000);
+
   // show starting time
+
+  timerEl.html = time ;
 
   getQuestion();
 }
 
 function getQuestion() {
   // get current question object from array
+  var currentQuestion = questions[currentQuestionIndex];
   // update title with current question
+  questionTitle.textContent = currentQuestion.title;
+
   // clear out any old question choices
+  choicesEl.textContent = " ";
   // loop over choices
+  currentQuestion.choices.forEach(function(choice, i){
   // create new button for each choice
   // attach click event listener to each choice
   // display on the page
+  var choiceNode = document.createElement("button");
+      choiceNode.setAttribute("class", "choice");
+      choiceNode.setAttribute("value", choice);
+  
+      choiceNode.textContent = i + 1 + ". " + choice;
+  
+      // attach click event listener to each choice
+      choiceNode.onclick = questionClick;
+  
+      // display on the page
+      choicesEl.appendChild(choiceNode);
+
+  })
+ 
+  
 }
 
 function questionClick() {
@@ -55,14 +88,22 @@ function questionClick() {
 
 function quizEnd() {
   // stop timer
-  // show end screen
+  clearInterval(timerId);
+  // show end screen;
   // show final score
+  endScreen.setAttribute("class" , "show");
   // hide questions section
+  questionScreen.setAttribute("class" , "hide");
 }
 
 function clockTick() {
   // update time
+  time-- ;
+  timerEl.textContent = time ;
   // check if user ran out of time
+  if ( time < 0 ){
+    quizEnd();
+  }
 }
 
 function saveHighscore() {
